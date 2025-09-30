62b8867fa50f983dd803a788e1482238077eca7c
Y0rFa1se <neargul123@gmail.com>
2025-09-30 19:54:34 +0900

    edit username

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,5 @@
 set -e
 
 cd actions-runner
-./config.sh --url https://github.com/${ORGANIZATION} --token ${TOKEN}
+./config.sh --url https://github.com/${ORGANIZATION} --token ${TOKEN} --name "${RUNNER_NAME}"
 exec ./run.sh
