f2ede8ee94dc17deccf795713d5961ca646e566b
Y0rFa1se <neargul123@gmail.com>
2025-10-02 15:46:37 +0900

    asdfg

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # self-host-runner
 
 Settings > Actions > Runners > New self-hosted runner
+
+- sudo podman 으로 실행해야함
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -e
 
-sudo chmod 666 /run/user/${HOST_UID}/podman/podman.sock
-
 cd actions-runner
 ./config.sh --url https://github.com/${ORGANIZATION} --token ${TOKEN} --name "${RUNNER_NAME}"
 exec ./run.sh
